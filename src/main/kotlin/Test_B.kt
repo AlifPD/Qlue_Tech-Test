cba1d3f4b3d45eb5bf2cb4bdf88b37a7c63d2b8a
Muhammad Alif Putra D <muhammadalifputradafi@gmail.com>
2022-07-30 17:19:15 +0700

    Test_B.kt finished

---
+++ src/main/kotlin/Test_B.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val data = arrayOf(1, 2, 3, 4, 5, 6)
+    multiplyIndex(data)
+}
+
+fun multiplyIndex(data: Array<Int>) {
+    val temp= mutableListOf<Int>()
+    for(i in data.indices){
+        temp.add(data[i]*data[data.size - i - 1])
+    }
+    println(temp.joinToString(","))
+}
