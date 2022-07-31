a9350f409875aa34ee4233e9c08bf8e2fd2a985d
Muhammad Alif Putra D <muhammadalifputradafi@gmail.com>
2022-07-31 09:20:02 +0700

    Final

--- src/main/kotlin/Test_A.kt
+++ src/main/kotlin/Test_A.kt
@@ -5,14 +5,13 @@ fun main(){
 
 fun sortData(data: Array<Int>){
     val ranges = 0 until data.size-2
-    var temp: Int
 
     for(i in ranges){
         for(j in ranges){
             if (data[j] > data[j + 1]) {
-                temp = data[j]
-                data[j] = data[j + 1]
-                data[j + 1] = temp
+                data[j] += data[j+1]
+                data[j+1] = data[j]-data[j+1]
+                data[j] -= data[j+1]
             }
         }
     }
